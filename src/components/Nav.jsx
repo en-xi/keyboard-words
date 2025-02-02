0bd38a152ba09d9a7ff6f1b53d0e44de79d3af15
hassan <enxi666freedom@gmail.com>
2025-02-02 16:28:46 +0800

    modify page design

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,26 @@
 import { Outlet, Link } from "react-router-dom";
+import { Footer } from "@/components/Footer.jsx";
 
 const Nav = () => {
   return (
     <>
-      <nav>
-        <ul className="flex items-center justify-center gap-x-4">
-          <li className="link">
+      <nav className="fixed w-full top-0 text-3xl bg-white">
+        <span className="fixed bg-green-300 ">For English Beginners</span>
+        <ul className="flex-center inline">
+          <li className="link inline">
             <Link to="/">Home</Link>
           </li>
-          <li className="link">
+          <li className="link inline">
             <Link to="/about">About</Link>
           </li>
         </ul>
       </nav>
 
-      <Outlet />
+      <div className="mt-10">
+        <Outlet />
+      </div>
+
+      <Footer></Footer>
     </>
   );
 };
